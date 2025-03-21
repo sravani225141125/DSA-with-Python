07e8ea1053bb0bab7f02a1ef035de91f03ca84e0
sravani225141125 <sravanikokkiligadda2219@gmail.com>
2025-03-21 17:58:02 +0530

    Update BigO(n^2).py

--- BigO(n^2).py
+++ BigO(n^2).py
@@ -1,3 +1,9 @@
+
+'''
+The code which is given below is the example of 
+O(n^2) 
+which is also known as loop within a loop
+'''
 def print_items(n) :
     for i in range(n):
         for j in range(n):
