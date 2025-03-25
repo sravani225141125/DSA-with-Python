ea0900e492f674af14f57f2a6c17fb692a0e0c77
sravani225141125 <sravanikokkiligadda2219@gmail.com>
2025-03-25 13:27:32 +0530

    Create DLL_constructor.py

---
+++ DLL_constructor.py
@@ -0,0 +1,34 @@
+class Node:
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    def __init__(self,value) :
+        self.value = value
+        self.next = None
+        self.prev =None
+        
+class DoublyLinkedList:
+    ## WRITE DLL CONSTRUCTOR HERE ##
+    def __init__(self,value) :
+        new_node=Node(value)
+        self.head = new_node
+        self.tail=new_node
+        self.length=1
+
+
+
+
+my_doubly_linked_list = DoublyLinkedList(7)
+
+print('Head:', my_doubly_linked_list.head.value)
+print('Tail:', my_doubly_linked_list.tail.value)
+print('Length:', my_doubly_linked_list.length)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 7
+    Tail: 7
+    Length: 1
+
+"""
